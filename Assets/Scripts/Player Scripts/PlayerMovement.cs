be32e265c034352108bb3c0c21a858b3d83f24ac
CoopCoup <r015829m@student.staffs.ac.uk>
2024-03-25 12:47:21 +0000

    Week 3a

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 inputVector;
+    private Animator animator;
     [SerializeField] private float moveSpeed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -28,5 +30,10 @@ public class PlayerMovement : MonoBehaviour
     private void MovePlayer()
     {
         rb.velocity = inputVector * moveSpeed;
+        if (inputVector != Vector2.zero)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else animator.SetBool("IsMoving", false);
     }
 }
